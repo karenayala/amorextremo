09089458d12ba3b5c837184aca81d8843dd3126c
unknown <kayala@DesarrolladorIT.parking2.local>
2018-01-31 17:36:38 -0500

    proyecto AmorExtremo t

---
+++ assets/js/app.js
@@ -0,0 +1,43 @@
+/*$.get("http://192.168.2.2:9081/mensuales/rest/tercero/iden/111", function(data, status){
+	console.log(data)
+    //alert("Data: " + data + "\nStatus: " + status);
+});
+*/
+/*
+$("#btn").click(function(){
+	
+	var data='{"id":1,"cliente":"555","estado":"SALIO","fecha":"2017-08-05","ping":"123","placa":"nni777","terminal":"K25B"}'
+	/*var data={}
+	data.id="1";
+	data.cliente="7777";
+	data.estado="SALIO";
+	data.fecha="2018-08-05";
+	data.ping="null";
+	data.placa="cvb123";
+	data.terminal="j56";*/
+	
+	/*
+	console.log(data);
+	*/
+	/*
+    $.post("http://192.168.2.2:9081/campanas/rest/emobi/setTransaccionEmobi",
+    data,
+	dataType: json,
+    function(response, status){        
+		console.log(response)
+    });
+	*/
+/*	$.ajax({
+	  type: "POST",
+	  url: "http://192.168.2.2:9081/campanas/rest/emobi/setTransaccionEmobi",
+	  data: data,
+	  contentType: "application/json; charset=utf-8", // this
+       dataType: "json", // and this
+	  success: function(response, status){        
+		console.log(response)
+    },
+	 
+	 
+	});
+});
+*/
